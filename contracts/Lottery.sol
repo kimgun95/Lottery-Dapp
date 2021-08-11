c132efc278f3f85e5fcf1d3cbaf3733f17c9aea4
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2021-08-11 18:01:13 +0900

    distribute 함수 구현

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -11,7 +11,7 @@ contract Lottery {
 		byte challenges; // 0xab
 	}
 
-	address public owner;
+	address payable public owner;
 
 	// map을 이용한 선형 queue 사용, 배팅 정보들을 저장
 	uint256 private _tail;
@@ -24,9 +24,17 @@ contract Lottery {
 	uint256 constant internal BET_AMOUNT = 5 * 10 ** 15; // 5 * 0.001ETH
 	uint256 private _pot; // 팟머니를 저장할 곳
 
+	bool private mode = false; // false : use answer for test , true : use real block hash
+    bytes32 public answerForTest;
+
 	enum BettingResult {Win, Fail, Draw}
 	enum BlockStatus {Checkable, NotRevealed, BlockLimitPassed}
+
 	event BET(uint256 index, address bettor, uint256 amount, byte challenges, uint256 answerBlockNumber);
+	event WIN(uint256 index, address bettor, uint256 amount, byte challenges, byte answer, uint256 answerBlockNumber);
+    event FAIL(uint256 index, address bettor, uint256 amount, byte challenges, byte answer, uint256 answerBlockNumber);
+    event DRAW(uint256 index, address bettor, uint256 amount, byte challenges, byte answer, uint256 answerBlockNumber);
+    event REFUND(uint256 index, address bettor, uint256 amount, byte challenges, uint256 answerBlockNumber);
 
 	constructor() public {
 		owner = msg.sender;
@@ -54,39 +62,106 @@ contract Lottery {
 	}
 
 	
-	// distribute
+	/**
+     * @dev 베팅 결과값을 확인 하고 팟머니를 분배한다.
+     * 정답 실패 : 팟머니 축척, 정답 맞춤 : 팟머니 획득, 한글자 맞춤 or 정답 확인 불가 : 베팅 금액만 획득
+     */
 	function distribute() public {
-		uint256 cur;
+		uint256 cur; // queue의 _head부터 ++되며 block을 탐색하는 index역할.
+		uint256 transferAmount; // 보내야할 금액.
+
 		BetInfo memory b;
-		BlockStatus currentBlockStatus;
+		BlockStatus currentBlockStatus; // 블럭을 체크할 수 있는지에 대한 상태.
+		BettingResult currentBettingResult; // 베팅에 대한 결과.
 
 		for (cur = _head; cur < _tail; cur++) {
 			b = _bets[cur];
 			currentBlockStatus = getBlockStatus(b.answerBlockNumber);
+
 			// Checkable: block.number > AnswerBlockNumber && block.number  <  BLOCK_LIMIT + AnswerBlockNumber
 			if (currentBlockStatus == BlockStatus.Checkable) {
+				bytes32 answerBlockHash = getAnswerBlockHash(b.answerBlockNumber);
+                currentBettingResult = isMatch(b.challenges, answerBlockHash);
+				
 				// if win, bettor gets pot
+				if(currentBettingResult == BettingResult.Win) {
+                    // transfer pot
+                    transferAmount = transferAfterPayingFee(b.bettor, _pot + BET_AMOUNT);
+                    // pot = 0
+                    _pot = 0;
+                    // emit WIN
+                    emit WIN(cur, b.bettor, transferAmount, b.challenges, answerBlockHash[0], b.answerBlockNumber);
+                }
 
 				// if fail, bettor's money goes pot
+				if(currentBettingResult == BettingResult.Fail) {
+                    // pot = pot + BET_AMOUNT
+                    _pot += BET_AMOUNT;
+                    // emit FAIL
+                    emit FAIL(cur, b.bettor, 0, b.challenges, answerBlockHash[0], b.answerBlockNumber);
+                }
 
 				// if draw, refund bettor's money
+				if(currentBettingResult == BettingResult.Draw) {
+                    // transfer only BET_AMOUNT
+                    transferAmount = transferAfterPayingFee(b.bettor, BET_AMOUNT);
+                    // emit DRAW
+                    emit DRAW(cur, b.bettor, transferAmount, b.challenges, answerBlockHash[0], b.answerBlockNumber);
+                }
 
 			}
+
 			// Not Revealed: block.number <= AnswerBlockNumber
 			if (currentBlockStatus == BlockStatus.NotRevealed) {
 				break;
 			}
+			
 			// Block Limit Passed: block.number >= AnswerBlockNumber + BLOCK_LIMIT
 			if (currentBlockStatus == BlockStatus.BlockLimitPassed) {
-				// refunc
-
-				// emit refund
+				// refund
+				transferAmount = transferAfterPayingFee(b.bettor, BET_AMOUNT);
+                // emit refund
+                emit REFUND(cur, b.bettor, transferAmount, b.challenges, b.answerBlockNumber);
 			}
 
 			popBet(cur);
 		}
+		// break 혹은 loop를 끝냈을 때 _head 값 설정.
+		_head = cur;
 	}
 
+	function transferAfterPayingFee(address payable addr, uint256 amount) internal returns (uint256) {
+        
+        // uint256 fee = amount / 100;
+        uint256 fee = 0;
+        uint256 amountWithoutFee = amount - fee;
+
+        // transfer to addr
+        addr.transfer(amountWithoutFee);
+
+        // transfer to owner
+        owner.transfer(fee);
+
+        return amountWithoutFee;
+    }
+
+	function setAnswerForTest(bytes32 answer) public returns (bool result) {
+        require(msg.sender == owner, "Only owner can set the answer for test mode");
+        answerForTest = answer;
+        return true;
+    }
+
+    function getAnswerBlockHash(uint256 answerBlockNumber) internal view returns (bytes32 answer) {
+        return mode ? blockhash(answerBlockNumber) : answerForTest;
+    }
+
+
+	/**
+     * @dev 베팅글자와 정답을 확인한다.
+     * @param challenges 베팅 글자
+     * @param answer 블락해쉬
+     * @return 정답결과
+     */
 	function isMatch(byte challenges, bytes32 answer) public pure returns (BettingResult) {
 		// challenges 0xab
 		// answer 0xab......ff 32 bytes
